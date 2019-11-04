197e5d7f69c916c3847c0883c1ff818761c0653a
emlycool <joshua.moshood@gmail.com>
2019-11-04 20:18:17 +0100

    db changed

--- engine/database.php
+++ engine/database.php
@@ -7,9 +7,9 @@ define('database', 'heR17wKBNs');
 $mycon= mysqli_connect(server, username, password, database);
 
 define('server2', 'remotemysql.com');
-define('username2', 'VeYKTw7cPi ');
-define('password2', 'YyZtc5rdQ5');
-define('database2', 'VeYKTw7cPi ');
+define('username2', 'VeYKTw7cPi');
+define('password2', 'MsE6GOmpi1');
+define('database2', 'VeYKTw7cPi');
 
 $mycon2= mysqli_connect(server2, username2, password2, database2);
 
